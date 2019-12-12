c21b29ddf8c7c73cfb93edde1c8ba746bb7f5ce1
Georgiy <g3orge.miller@gmail.com>
2019-12-12 18:40:42 +0500

    working on independent movie component

--- frontend/components/independent_movie/IndependentMovie.jsx
+++ frontend/components/independent_movie/IndependentMovie.jsx
@@ -6,27 +6,31 @@ import Page from "../page/Page";
 
 class IndependentMovie extends React.Component {
   render() {
-    const movie = this.props.movie;
+    const fields = this.props.fields;
     return (
       <Page>
         <div className="independent-movie flex-container">
           <div className="left-side">
-            <img src={movie.cover.main.url} className="independent-movie--cover" />
+            <img
+              src={this.props.cover.main.url}
+              className="independent-movie--cover"
+            />
           </div>
 
           <div className="right-side">
-            <h1> {movie.title} </h1>
-            {
-              Object.values(movie).map(function(prop, i) {
-                if (typeof(prop) != "object") {
-                  return <div className="independent-movie--field" key={i}> {prop} </div>
-                }
-              })
-            }
+            <h1 className="independent-movie--title"> {this.props.title} </h1>
+            {Object.entries(fields).map(([key, value]) => {
+              return (
+                <div className="independent-movie--field" key={key}>
+                  <label className="float-label"> {key} </label>
+                  <span> {value} </span>
+                </div>
+              );
+            })}
           </div>
         </div>
       </Page>
-    )
+    );
   }
 }
 
