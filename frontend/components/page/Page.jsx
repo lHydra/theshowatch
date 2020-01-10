871304878227f8a15c57671bb3f93c05f6e18a5c
Georgiy <g3orge.miller@gmail.com>
2020-01-10 17:25:12 +0500

    collapsed navbar in mobile version

--- frontend/components/page/Page.jsx
+++ frontend/components/page/Page.jsx
@@ -7,42 +7,37 @@ class Page extends React.Component {
     super();
     this.state = {
       currentUser: null
-    }
+    };
     this.updateCurrentUser = this.updateCurrentUser.bind(this);
   }
 
   updateCurrentUser(email) {
     this.state = {
       currentUser: email
-    }
+    };
   }
 
   componentDidMount() {
-    fetch('/users/check_for_user', {
-      method: 'GET',
+    fetch("/users/check_for_user", {
+      method: "GET"
     })
-    .then(response => response.json())
-    .then(result => {
-      console.log(result.email);
-      if (result.email) {
-        this.setState({
-          currentUser: result.email
-        })
-      } else {
-        this.setState({
-          currentUser: null
-        })
-      }
-    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.email) {
+          this.setState({
+            currentUser: result.email
+          });
+        } else {
+          this.setState({
+            currentUser: null
+          });
+        }
+      });
   }
 
-  render () {
-    return (
-      <div className="container page">
-        {this.props.children}
-      </div>
-  );
+  render() {
+    return <div className="container page">{this.props.children}</div>;
   }
 }
 
-export default Page
+export default Page;
